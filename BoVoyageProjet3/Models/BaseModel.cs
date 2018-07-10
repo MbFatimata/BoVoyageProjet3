a002fb61d920ec3f1cc56a246b4aa0c827b10f3c
Fati <Adminl@EMER06>
2018-07-10 09:08:20 +0200

    creation projet BoVoyage creation classe BaseModel

---
+++ BoVoyageProjet3/Models/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoVoyageProjet3.Models
+{
+    public abstract class BaseModel
+    {
+        //[Key]
+        public int ID { get; set; }
+    }
+}
